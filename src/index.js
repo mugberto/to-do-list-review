a092f426eb4792da615e188a02b3bd1a9dbc5d6d
Hubert MUGABO <mugberto@gmail.com>
2021-07-14 16:25:38 +0200

    Add taskList and a method to display it

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
 import './style.css';
+
+let taskList = [
+  {
+    index: 1,
+    description: 'Shopping groceries',
+    completed: true
+  },
+  {
+    index: 2,
+    description: 'Dinner with friends',
+    completed: false
+  },
+  {
+    index: 3,
+    description: 'Finish assignment',
+    completed: true
+  },
+  {
+    index: 4,
+    description: 'Jogging',
+    completed: false
+  },
+];
+
+const displayTaskList = function(list) {
+  const ul = document.getElementById('list-id');
+  list.forEach( task => { 
+    const li = document.createElement('li');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    if(task.completed){
+      checkbox.setAttribute('checked', 'checked');
+    }
+    li.appendChild(checkbox);
+    li.innerHTML += `<span>${task.description}</span> <i class="fa fa-ellipsis-v"></i>`;
+    ul.appendChild(li);
+  });
+}
+
+displayTaskList(taskList);
