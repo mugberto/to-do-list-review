a3a709b551f32bbecca2ce0bcd61d8fbecd8b042
Hubert MUGABO <mugberto@gmail.com>
2021-07-17 12:01:35 +0200

    Add taskList.delete method

--- src/taskList.js
+++ src/taskList.js
@@ -25,4 +25,15 @@ export default class TaskList {
     });
     localStorage.setItem('tasks', JSON.stringify(this.list));
   }
+
+  delete(index) {
+    this.list = this.list.filter(task => task.index !== index);
+    this.list = this.list.map(task => {
+      if(task.index > index ){
+        task.index -= 1;
+      }
+      return task;
+    });
+    localStorage.setItem('tasks', JSON.stringify(this.list));
+  }
 }
